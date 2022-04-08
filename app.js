4df6bc557a3e51047ac71c0a9a1369af807d2bd1
Jared <jkpeople1856@gmail.com>
2022-04-08 10:37:52 -0500

    finished exercise

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-// comment
+const plus = (num) => {
+    return (plusNumber) => (plusNumber += num);
+};
+    let plus15 = plus(15);
+
+console.log(plus15(10)) 
